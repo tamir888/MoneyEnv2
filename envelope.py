808a0c13185046c6abb9532e013c5555ecce17eb
Liri Benzinou <71220474+thatmathnerd@users.noreply.github.com>
2020-10-10 13:27:46 +0300

    Add files via upload

--- envelope.py
+++ envelope.py
@@ -6,3 +6,5 @@ class Envelope:
         self.money = int(random.randint(0, 10000000000))
         self.used = False
 
+    def display(self):
+        return str(self.money) + "dollars"
--- strategy.py
+++ strategy.py
@@ -1,4 +1,5 @@
 from envelope import Envelope
+import random
 
 
 class BaseStrategy:
@@ -11,6 +12,23 @@ class Automatic_BaseStrategy(BaseStrategy):
         super().__init__(envelopes)
         self.envelopes = envelopes
 
+    def play(self):
+        """
+        play the game: take random envelope from the list of envelopes and print the amount of money
+        :return none:
+        """
+        num = random.randrange(100)
+        envelope = self.envelopes[num]
+        print(envelope.display())
+
+    def display(self):
+        """
+        display what this strategy is doing
+        :return description:
+        """
+        return "Automatic BaseStrategey - take random envelope from the list of envelopes \n " \
+               "and print how much money was in the envelope and that's how much you've got."
+
 
 class N_max_strategy(BaseStrategy):
     def __init__(self, envelopes):
@@ -21,19 +39,21 @@ class N_max_strategy(BaseStrategy):
     def play(self):
         """
         play the game: open the envelopes until we find N envelopes with max money and then stop
-        and print the amount of money
+                       and print the amount of money
         :return none:
         """
         max_money = 0
         count = 0
         index = 0
+        max_env = self.envelopes[0]
         while index < 100 and count < self.N:
             if self.envelopes[index].money > max_money and not self.envelopes[index].used:
                 max_money = self.envelopes[index].money
                 count += 1
-                self.envelopes[index].used = True
+                max_env = self.envelopes[index]
+            self.envelopes[index].used = True
             index += 1
-        print("you got: " + str(max_money) + " dollars")
+        print(max_env.display())
 
     def display(self):
         """
@@ -48,3 +68,39 @@ class More_then_N_percent_group_strategy(BaseStrategy):
         super().__init__(envelopes)
         self.envelopes = envelopes
         self.percent = percent
+
+    def play(self):
+        """
+        play the game: opens x% of envelopes and chooses the envelope with the highest amount of money inside.
+                       Then it compares the amount of money to the other 100% - x% of the envelopes and if a
+                       higher amount is found it will replace the envelope and immediately print the amount of
+                       money of the new envelope
+        :return None:
+        """
+        opened_env = 0
+        highest_amount = self.envelopes[opened_env].money
+        opened_env = 1
+        absolute_highest_amount = 0
+        env_to_return = None
+        while (self.percent * 100) > opened_env:
+            if self.envelopes[opened_env].money > highest_amount:
+                highest_amount = self.envelopes[opened_env].money
+                env_to_return = self.envelopes[opened_env]
+                self.envelopes[opened_env - 1].used = True
+                opened_env = opened_env + 1
+        absolute_highest_amount = highest_amount
+        while opened_env < 100:
+            if self.envelopes[opened_env].money > absolute_highest_amount:
+                absolute_highest_amount = self.envelopes[opened_env].money
+                env_to_return = self.envelopes[opened_env]
+                break
+            self.envelopes[opened_env].used = True
+            opened_env = opened_env + 1
+        print(env_to_return.displey())
+
+    def display(self):
+        return (" opens x% of envelopes and chooses the envelope \n "
+                "with the highest amount of money inside. then it \n "
+                "compares the amount of money to the other 100% - x% \n "
+                "of the envelopes and if a higher amount is found \n"
+                "it will replace the envelope and immediately return the new envelope")
